17ac6b37dd24ad0a787f325203edaffda4c56e15
abidtalukdar <abiditalukdar@gmail.com>
2020-03-06 16:11:35 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Write your code here
+
+let var1 = 31
+let var2 = 2
+
+const multiply = var1 * var2;
+
+const random = Math.random()
+
+const mod = 20 % 8
+
+const max = Math.max(1, 2, 3, 4, 20)
